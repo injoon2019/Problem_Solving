6fc053c1ab2c9680f001f507655e072aebcb6d90
Injun Son <injoon2019@gmail.com>
2020-03-03 17:04:36 -0800

    daily

--- src/BaekJoon9465.java
+++ src/BaekJoon9465.java
@@ -24,21 +24,14 @@ public class BaekJoon9465 {
 			dp[0][1]= arr[0][0];
 			dp[1][1]= arr[1][0];
 			
+			//한 열을 뛰어 넘을 수도 있다. 바로 전 열의 대각선, 또는 전전열에서의 대각선 경우를 생각할 수 있다.
 			for(int k=2; k<=n;k++) {
 				dp[0][k] = Math.max(dp[1][k-1], dp[1][k-2]) + arr[0][k-1];
 				dp[1][k] = Math.max(dp[0][k-1], dp[0][k-2]) + arr[1][k-1];
 			}
 			
 			long max = dp[0][0];
-			for(int k=0; k<2; k++) {
-				for(int j=0; j<=n; j++) {
-					System.out.printf("%d ", dp[k][j]);
-				}
-				System.out.println();
-			}
-			
-			
-			
+						
 			for(int k=0; k<2; k++) {
 				for(int j=1; j<=n; j++) {
 					max = Math.max(max, dp[k][j]);
