12ba5ddff7160c09668cfd254da6fabb0015b573
Injun Son <injoon2019@gmail.com>
2020-03-07 01:11:08 -0800

    Mar 7, 2020

---
+++ src/BaekJoon1912.java
@@ -0,0 +1,32 @@
+//Baekjoon - 1912
+
+import java.io.*;
+public class BaekJoon1912 {
+	
+	public static void main(String[] args)throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		String[] strToken = br.readLine().split(" ");
+		int[] arr = new int[N];
+		int[] dp = new int[N];
+		for(int i=0; i<N; i++) {
+			arr[i]= Integer.parseInt(strToken[i]);
+		}
+		
+		dp[0]=arr[0];
+		for(int i=1; i<N; i++) {
+			if(dp[i-1]<0) {
+				 dp[i]=arr[i];
+			}
+			if(dp[i-1]>=0) {
+				dp[i] = dp[i-1]+arr[i];
+			}
+		}
+		int max = dp[0];
+		for(int i=0; i<N; i++) {
+			max= Math.max(max, dp[i]);
+		}
+		System.out.println(max);
+	}	
+}
