1f12e1523441e6b56adf3bb58402974620cd83de
Injun Son <injoon2019@gmail.com>
2020-03-13 11:36:48 -0700

    2011

---
+++ src/BaekJoon2011.java
@@ -0,0 +1,40 @@
+//Baekjoon - 2011
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BaekJoon2011 {
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String N = br.readLine();
+		
+		long[] dp = new long[N.length()+1];
+		dp[0]=1;
+		dp[1]=1;
+		
+		if(N.charAt(0)=='0') {
+			System.out.println(0);
+			return;
+		}
+		
+		for(int i=1; i<N.length(); i++) {
+			char pri = N.charAt(i-1);//이전 숫자
+			if(N.charAt(i)>='1'&& N.charAt(i)<='9')//혼자 올 수 있음
+			{
+				dp[i+1] +=dp[i];
+				dp[i+1] %=  1000000;
+			}
+			if(!(pri=='0'||pri>'2'||(pri=='2'&&N.charAt(i)>'6')))
+			{	//두 글자가 될 수 있다면
+				dp[i+1] +=dp[i-1];
+				dp[i+1]%= 1000000;
+				
+			}
+		}
+		
+		System.out.println(dp[N.length()]%1000000);
+		
+	}
+}
