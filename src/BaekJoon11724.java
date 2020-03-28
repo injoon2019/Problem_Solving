b8fc632884c433ff6ecc586fec786b09367bfa44
Injun Son <ison@sfu.ca>
2020-03-27 22:14:20 -0700

    BFS 11724

--- src/BaekJoon11724.java
+++ src/BaekJoon11724.java
@@ -1,28 +1,26 @@
-//Baekjoon - 2004
-//https://www.geeksforgeeks.org/count-trailing-zeroes-factorial-number/
+//Baekjoon - 11724
 import java.io.*;
 import java.util.*;
 //DFS, BFS를 인접행렬로 구현하면 시간복잡도가 O(V2)이지만, 
 //인접리스트로 구현하면 O(V+E)가 됩니다. (V는 정점 개수, E는 간선 개수)
-public class BaekJoon2004 {
+public class BaekJoon11724 {
 	
 	static ArrayList<Integer>[] list;
-	static int n;
 	static boolean[] check;
+	static boolean[] check2;
+	static int count = 0;
 	
 	public static void main(String[] args)throws Exception{
 		Scanner sc = new Scanner(System.in);
-		n = sc.nextInt();
-		int m = sc.nextInt();
-		int start = sc.nextInt();
+		int N = sc.nextInt();
+		int M = sc.nextInt();
 		
-		list = new ArrayList[n+1];
-		
-		for(int i=1; i<n+1; i++) {
-			list[i] = new ArrayList<Integer>();
+		list = new ArrayList[N+1];
+		for(int i=1;  i<=N; i++) {
+			list[i] =  new ArrayList<Integer>();
 		}
 		
-		for(int i=0; i<m; i++) {
+		for(int i=0; i<M; i++) {
 			int u = sc.nextInt();
 			int v = sc.nextInt();
 			
@@ -30,40 +28,29 @@ public class BaekJoon2004 {
 			list[v].add(u);
 		}
 		
-		for(int i=1; i<n+1; i++) {
+		for(int i=1; i<=N; i++) {
 			Collections.sort(list[i]);
 		}
-		check = new boolean[n+1];
-		dfs(start);
-		System.out.println();
-		
-		check = new boolean[n+1];
-		bfs(start);
-		System.out.println();
-		
+		check = new boolean[N+1];
+		check2 = new boolean[N+1];
+		int count = 0;
+		for(int i=1;  i<=N; i++) {
+			if(!check[i]) {
+				bfs(i);
+				count++;
+			}
+		}
+		System.out.println(count);
 		sc.close();
 	}	
-	
-	private static void dfs(int x) {
-		if(check[x]) {
-			return;
-		}
-		check[x]= true;
-		System.out.print(x+" ");
-		for(int y : list[x]) {
-			if(!check[y])
-				dfs(y);
-		}
-	}
 	
 	private static void bfs(int start) {
 		Queue<Integer> queue = new LinkedList<Integer>();
 		queue.add(start);
-		check[start] = true;
+		check[start]= true;
 		
 		while(!queue.isEmpty()) {
 			int x = queue.poll();
-			System.out.print(x+" ");
 			for(int y : list[x]) {
 				if(!check[y]) {
 					check[y]=true;
@@ -72,4 +59,6 @@ public class BaekJoon2004 {
 			}
 		}
 	}
+	
+
 }
