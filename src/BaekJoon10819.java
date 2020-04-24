2b57c87b16330274816bc8412b3695d383e168e5
Injun Son <ison@sfu.ca>
2020-04-24 05:27:05 -0700

    permutation

--- src/BaekJoon10819.java
+++ src/BaekJoon10819.java
@@ -1,41 +1,51 @@
-//Baekjoon - 10819
-
+//Baekjoon - 10819 차이를 최대로 (순열!)
+//https://gorakgarak.tistory.com/522
 import java.io.*;
+import java.util.StringTokenizer;
 public class BaekJoon10819 {
+	static int max=0;
+	public static void perm(int[] a, int depth, int n) {
+		if(depth==n) {
+			sum(a,n);
+			return;
+		}
+		//depth가 0이라면 1xxx, 2xxx, 3xxx, 4xxx 네번 돈다
+		for(int i=depth; i<n; i++) {
+			swap(a, i, depth);
+			perm(a, depth+1, n);
+			//두번째 swap()은 전단계의 분기점에서의 배열의 순서를 기억하고 이를 초기화하는 작업
+			swap(a, i, depth);
+		}
+	}
+	
+	static void swap(int[] a, int depth, int n) {
+		int temp = a[depth];
+		a[depth] = a[n];
+		a[n] = temp;
+	}
+	
+	static void sum(int[] a, int k) {
+		int sum = 0;
+		for(int i=2; i<=k; i++) {
+			sum += Math.abs(a[i-2]-a[i-1]);
+		}
+		if(max<sum) {
+			max = sum;
+		}
+	}
 	
 	public static void main(String[] args)throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		int N = Integer.parseInt(br.readLine());
-		int[] arr = new int[N];
-		String[] strToken = br.readLine().split(" ");
+		int[] num = new int[N];
+		String number = br.readLine();
+		StringTokenizer st = new StringTokenizer(number, " ");
 		for(int i=0; i<N; i++) {
-			arr[i]= Integer.parseInt(strToken[i]);
+			num[i] = Integer.parseInt(st.nextToken());
 		}
-		
-		int max = Integer.MIN_VALUE;
-		int tmp = 0;
-		
-		for(int i=0; i<arr.length-1; i++) {
-			for(int j=i+1; j<arr.length; j++) {
-				tmp= arr[j];
-				arr[j] = arr[i];
-				arr[i]=tmp;
-				int cur = arrSum(arr);
-				if(cur>max)
-					max=cur;
-			}
-		}
+		perm(num, 0, N);
 		System.out.println(max);
-		
 	}	
-	
-	public static int arrSum(int[] arr) {
-		int sum=0;
-		for(int i=0; i<arr.length-1; i++) {
-			sum += Math.abs(arr[i]-arr[i+1]);
-		}
-		return sum;
-	}
 }
---
+++ src/BaekJoon10971.java
@@ -0,0 +1,68 @@
+//Baekjoon - 10971 외판원 순회 2
+import java.io.*;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+public class BaekJoon10971 {
+
+	static int min = Integer.MAX_VALUE;
+	static int[][] cost;
+	public static void perm(int[] arr, int depth, int n) {
+		if(depth==n) {
+			sum(arr);
+			return;
+		}
+		
+		for(int i=depth; i<n; i++) {
+			swap(arr, i, depth);
+			perm(arr, depth+1, n);
+			swap(arr, i, depth);
+		}
+	}
+	
+	static void swap(int[] a, int depth, int n) {
+		int temp = a[depth];
+		a[depth] = a[n];
+		a[n] = temp;
+	}
+	
+	static void sum(int[] a) {
+		int sum = 0;
+		for(int i=0; i<a.length-1; i++) {
+			sum += cost[a[i]][a[i+1]];
+			if(cost[a[i]][a[i+1]]==0) {
+				sum = Integer.MAX_VALUE;
+			}
+		}
+		if(cost[a.length-1][a[0]]==0) {
+			sum = Integer.MAX_VALUE;
+		}else {
+			sum += cost[a[a.length-1]][a[0]];
+		}
+		if(sum < min) {
+			min = sum;
+		}
+	}
+	
+	public static void main(String[] args)throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
+		String[] strToken;
+		
+		cost = new int[N][N];
+		for(int i=0; i<N; i++) {
+			strToken = br.readLine().split(" ");
+			for(int j=0; j<N; j++) {
+				cost[i][j] = Integer.parseInt(strToken[j]);
+			}
+		}
+		
+		int[] arr = new int[N];
+		for(int i=0; i<N; i++) {
+			arr[i] = i;
+		}
+		perm(arr, 0, N);
+		System.out.println(min);
+	}	
+}
