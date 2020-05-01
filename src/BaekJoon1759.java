640dcd6c3e8f5754c92147fed742839d745bddec
Injun Son <ison@sfu.ca>
2020-05-01 00:37:08 -0700

    Udemy, Section 7 Done

--- src/BaekJoon1759.java
+++ src/BaekJoon1759.java
@@ -4,32 +4,56 @@ import java.util.*;
 
 public class BaekJoon1759 {
 
-	static int N, M;
 	static char[] arr;
 	static int[] result;
-
+	static int N;
+	static int M;
+	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] strToken = br.readLine().split(" ");
-
-		N = Integer.parseInt(strToken[0]);
-		M = Integer.parseInt(strToken[1]);
+		String[] str = br.readLine().split(" ");
+		N = Integer.parseInt(str[0]);
+		M = Integer.parseInt(str[1]);
 		arr = new char[M];
 		result = new int[M];
-		
-		strToken = br.readLine().split(" ");
+		str = br.readLine().split(" ");
 		for(int i=0; i<M; i++) {
-			arr[i] = strToken[i].charAt(0);
+			arr[i] = str[i].charAt(0);
 		}
+		
 		Arrays.parallelSort(arr);
 		DFS(0,0,0,0);
 	}
 	
-	//시작점, 선택된 문자 갯수, 자음 갯수, 모음 갯수
+	//시작점 , 선택된 문자갯수, 자음갯수, 모음갯수
 	public static void DFS(int start, int depth, int ja, int mo) {
 		
 		for(int i=start; i<M; i++) {
-			result[i] = 1;	//선택된 문자 확인용
+			result[i]=1;	//선택된 문자 확인용
+			
+			DFS(i+1, depth+1, ja+(check(arr[i]) ? 0:1), mo+(check(arr[i])? 1:0));
+			result[i] = 0;	//0이면 선택 안됨, 백트랙킹 
 		}
+		//문자갯수가 N개이며 자음과 모음의 갯수가 규칙에 맞을때만 출력한다
+		if(depth==N && ja>=2 && mo>=1) {
+			print();
+		}
+	}
+	
+	public static void print() {
+		for(int i=0; i<M; i++) {
+			//result가 0이라면 선택되지 않았기 때문에 넘긴다
+			if(result[i]==1)
+				System.out.print(arr[i]);
+		}
+		System.out.println();
+	}
+	
+	//자음 모음 파악 함수
+	public static boolean check(char a) {
+		if(a=='a' || a=='e' || a=='i' || a=='o'|| a=='u')
+			return true;
+		else
+			return false;
 	}
 }
